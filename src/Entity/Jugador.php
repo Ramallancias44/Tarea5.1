2af2a634521dce43effb12df2ec2c5140f356048
Ramallancias44 <raulramallo123@gmail.com>
2025-06-03 22:02:59 +0200

    Commit2

---
+++ src/Entity/Jugador.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JugadorRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: JugadorRepository::class)]
+class Jugador
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\Column]
+    private ?float $altura = null;
+
+    #[ORM\Column]
+    private ?int $dorsal = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $fechaCreacion = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $fechaModificacion = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getAltura(): ?float
+    {
+        return $this->altura;
+    }
+
+    public function setAltura(float $altura): static
+    {
+        $this->altura = $altura;
+
+        return $this;
+    }
+
+    public function getDorsal(): ?int
+    {
+        return $this->dorsal;
+    }
+
+    public function setDorsal(int $dorsal): static
+    {
+        $this->dorsal = $dorsal;
+
+        return $this;
+    }
+
+    public function getFechaCreacion(): ?\DateTimeInterface
+    {
+        return $this->fechaCreacion;
+    }
+
+    public function setFechaCreacion(?\DateTimeInterface $fechaCreacion): static
+    {
+        $this->fechaCreacion = $fechaCreacion;
+
+        return $this;
+    }
+
+    public function getFechaModificacion(): ?\DateTimeInterface
+    {
+        return $this->fechaModificacion;
+    }
+
+    public function setFechaModificacion(?\DateTimeInterface $fechaModificacion): static
+    {
+        $this->fechaModificacion = $fechaModificacion;
+
+        return $this;
+    }
+}
